01f501b48b83fa523160c0423cf896a51b0a25cc
Dimitrij <150205312+dimitrij-ilijevski-milanovski@users.noreply.github.com>
2023-12-07 13:56:38 +0100

    Add files via upload

---
+++ Homework 2 (exercise)/app.js
@@ -0,0 +1,14 @@
+console.log("ALIVE!");
+let userInput = prompt("How much money do you have?");
+let numberResult = Number(userInput);
+let parseIntResult = parseInt(userInput);
+console.log(userInput, parseIntResult, numberResult);
+if (Number.isNaN(numberResult)) {
+  console.log("Invalid input, Enter NUMBERS ONLY!");
+} else if (numberResult >= 35) {
+  console.log("Go to the CLUB!!!  ");
+} else if (numberResult >= 25) {
+  console.log("Go to a restaurant!");
+} else if (numberResult >= 15) {
+  console.log("Buy PLASET!");
+} else console.log("Go study to earn money!!!!");
